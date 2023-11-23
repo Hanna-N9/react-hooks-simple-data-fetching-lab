88227c3ea83c7e839d8d4691a841023972bb8546
Hanna Negash <hnegash.n@gmail.com>
2023-11-22 21:07:01 -0800

    Work with  useEffect hook to fetch data from an API and use state

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [dog, setDog] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const dogAPI = "https://dog.ceo/api/breeds/image/random";
+
+  useEffect(() => {
+    fetch(dogAPI)
+      .then(res => res.json())
+      .then(data => {
+        setDog(data.message);
+        setIsLoading(true);
+      });
+  }, []);
+
+  //Display a <p> tag with the text of "Loading..." when the component is first rendered - if data hasn't been rendered, show an indicator
+  if (!isloading) return <p>Loading...</p>;
+
+  return <img src={dog} alt="A Random Dog" />;
+}
